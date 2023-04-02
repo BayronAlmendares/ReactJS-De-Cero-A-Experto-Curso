37c289d81592d7b73da4a4bcd3a236d43051a275
Bayron-Almendares <ing.bayronalmendares@gmail.com>
2023-04-01 20:59:42 -0600

    cumtom hook with return of funtions

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -5,8 +5,25 @@ import React, { useState } from 'react'
 export const useCounter = ( initialValue = 10) => {
   const [counter, setCounter] =  useState(initialValue);
 
+
+  const increment = () =>{
+    setCounter( counter + 1);
+  } 
+
+  const decrement = () =>{
+    if(counter === 0) return;
+    setCounter( counter - 1);
+  } 
+  
+  const reset = () =>{
+    setCounter(initialValue);
+  }
+
   //here I am sending an object
     return {
         counter : counter,
+        increment,
+        decrement,
+        reset,
     }
 }
